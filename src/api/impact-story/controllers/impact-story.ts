6d96109aa101e61e211b197229a4f71186862304
abdoulbinjim <abdulqudusjmh@gmail.com>
2025-10-08 15:32:34 +0100

    Initial commit - Strapi backend

---
+++ src/api/impact-story/controllers/impact-story.ts
@@ -0,0 +1,7 @@
+/**
+ * impact-story controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::impact-story.impact-story');
---
+++ src/api/impact-story/routes/impact-story.ts
@@ -0,0 +1,7 @@
+/**
+ * impact-story router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::impact-story.impact-story');
